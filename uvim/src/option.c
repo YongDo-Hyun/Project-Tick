23a2da1b6b4ddec905f28dad1c33002a4409cab0
Mehmet Samet Duman <yongdohyun@projecttick.org>
2026-03-22 22:27:46 +0300

    ci + uvim: fix GitHub App token source and improve backspace defaults
    - CI: use secrets.PULL_REQUEST_APP_ID instead of vars
      to ensure proper authentication for GitHub App token generation
    
    - uvim: set default 'backspace' option to "indent,eol,start"
      aligning behavior with modern Vim defaults
    
    Signed-off-by: Mehmet Samet Duman <yongdohyun@projecttick.org>

--- uvim/src/option.c
+++ uvim/src/option.c
@@ -576,7 +576,7 @@ static struct vimoption options[] =
 					    (char_u *)0L} SCRIPTID_INIT},
     {"backspace",   "bs",   P_STRING|P_VI_DEF|P_VIM|P_ONECOMMA|P_NODUP,
 			    (char_u *)&p_bs, PV_NONE,
-			    {(char_u *)"", (char_u *)0L} SCRIPTID_INIT},
+			    {(char_u *)"", (char_u *)"indent,eol,start"} SCRIPTID_INIT},
     {"backup",	    "bk",   P_BOOL|P_VI_DEF|P_VIM,
 			    (char_u *)&p_bk, PV_NONE,
 			    {(char_u *)FALSE, (char_u *)0L} SCRIPTID_INIT},
